188a90115203b23a953c818d36424c5e7cef5ee7
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-09 23:31:44 +0530

    Add files via upload

---
+++ uc1.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //computaition
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ uc3.cs
@@ -0,0 +1,36 @@
+﻿namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONSTANTS
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //VARIABLES
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            //COMPUTATION
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 16;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+
+    }
+}
